2d10a759a88901dd4821412e40aaf7689bfeb02b
Thuban Arrakis <thuban.arrakis@gmail.com>
2023-09-24 22:17:03 +0300

    chore(operator): sts configmap mount

--- controllers/statefulset.go
+++ controllers/statefulset.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
   "context"
+  "fmt"
+  "reflect"
 
   apps "k8s.io/api/apps/v1"
   corev1 "k8s.io/api/core/v1"
@@ -13,9 +15,8 @@ import (
 )
 
 func (r *PhareReconciler) reconcileStatefulSet(ctx context.Context, req ctrl.Request, phare pharev1beta1.Phare) (ctrl.Result, error) {
-  // Your existing logic for handling StatefulSet
-  statefulSet := &apps.StatefulSet{}
-  err := r.Get(ctx, req.NamespacedName, statefulSet)
+  existingStatefulSet := &apps.StatefulSet{}
+  err := r.Get(ctx, req.NamespacedName, existingStatefulSet)
   if err != nil && errors.IsNotFound(err) {
     // Before creating a new StatefulSet:
     phare.Status.Phase = PharePhaseReconciling
@@ -42,7 +43,14 @@ func (r *PhareReconciler) reconcileStatefulSet(ctx context.Context, req ctrl.Req
     return ctrl.Result{}, err
   }
 
-  // TODO: Update the StatefulSet if necessary...
+  desired := r.desiredStatefulSet(&phare)
+  if !reflect.DeepEqual(existingStatefulSet.Spec, desired.Spec) {
+    existingStatefulSet.Spec = desired.Spec
+    if err := r.Update(ctx, existingStatefulSet); err != nil {
+      return ctrl.Result{}, err
+    }
+  }
+
   return ctrl.Result{}, nil
 }
 
@@ -53,10 +61,7 @@ func (r *PhareReconciler) desiredStatefulSet(phare *pharev1beta1.Phare) *apps.St
     "app": phare.Name,
   }
 
-  // log := r.Log.WithValues("phare", phare.Name)
-  // log.Info("Generating desired StatefulSet for Phare", "Image", phare.Spec.Microservice.Image.Repository+":"+phare.Spec.Microservice.Image.Tag, "ReplicaCount", replicaCount)
-
-  return &apps.StatefulSet{
+  statefulSet := &apps.StatefulSet{
     ObjectMeta: metav1.ObjectMeta{
       Name:      phare.Name,
       Namespace: phare.Namespace,
@@ -81,4 +86,39 @@ func (r *PhareReconciler) desiredStatefulSet(phare *pharev1beta1.Phare) *apps.St
       },
     },
   }
+
+  // Check if the spec.config is not empty
+  if phare.Spec.Config != nil && len(phare.Spec.Config) > 0 {
+    // Add ConfigMap as a volume to the Pod template
+    statefulSetVolume := corev1.Volume{
+      Name: "config-volume",
+      VolumeSource: corev1.VolumeSource{
+        ConfigMap: &corev1.ConfigMapVolumeSource{
+          LocalObjectReference: corev1.LocalObjectReference{
+            Name: phare.Name + "-config",
+          },
+        },
+      },
+    }
+    configMapDataHash, err := r.hashConfigMapData(phare.Name+"-config", phare.Namespace)
+    if err != nil {
+      fmt.Println("Error hashing config map data")
+      // Handle error, maybe return an error or log it
+    }
+
+    if statefulSet.Annotations == nil {
+      statefulSet.Annotations = make(map[string]string)
+    }
+    statefulSet.Annotations["config-hash"] = configMapDataHash
+    statefulSet.Spec.Template.Spec.Volumes = append(statefulSet.Spec.Template.Spec.Volumes, statefulSetVolume)
+
+    // Mount the volume to the container
+    volumeMount := corev1.VolumeMount{
+      Name:      "config-volume",
+      MountPath: "/path/to/mount", // Adjust this to your desired mount path
+    }
+    statefulSet.Spec.Template.Spec.Containers[0].VolumeMounts = append(statefulSet.Spec.Template.Spec.Containers[0].VolumeMounts, volumeMount)
+  }
+
+  return statefulSet
 }
